9bde2afbaf56ba23577e8e5a8d876e0aa44e3a15
taban <cosmostaban@hotmail.com>
2014-11-06 19:55:05 -0800

    updates

--- web/index.php
+++ web/index.php
@@ -1,22 +1,3 @@
 <?php
 
-require('../vendor/autoload.php');
-
-$app = new Silex\Application();
-$app['debug'] = true;
-
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-
-// Our web handlers
-
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return '<h1>' . 'Whatsonplate is under construction come back soon!' . '</h1>';
-});
-
-$app->run();
-
-?>
+echo 'Test project';
