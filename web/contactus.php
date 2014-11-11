d2dc8ab912ac5b62c39d12cdf90aa3ad9d794777
taban <cosmostaban@hotmail.com>
2014-11-10 16:24:59 -0800

    updates

--- web/contactus.php
+++ web/contactus.php
@@ -1,28 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>WhatsonPlate</title>
-    <link href="images/favicon.ico" rel="icon" type="image/x-icon" />
-    <!-- Stylesheet -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/main.css">
-    <!-- font -->
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400' rel='stylesheet' type='text/css'>
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
+<?php require_once 'layout/heaeder.php'; ?>
+
       <div class="container">
-        <div class="row" style="text-align:center;margin-top:20px;">      
+
+        <div class="row" style="text-align:center;margin-top:20px;">    
+
           <?php
-            $EmailTo = "YOUR@EMAIL.COM";
+
+            $EmailTo = "taban@tabancosmos.com";
             $Subject = "Site Contact";
             $Name = Trim(stripslashes($_POST['Name'])); 
             $Email = Trim(stripslashes($_POST['Email'])); 
@@ -42,6 +26,7 @@
 
             // send email 
             $success = mail($EmailTo, $Subject, $Body, "From: <$Email>");
+            
             if ($_POST['submit']) {
                 if ($Name != '' && $Email != '' && $Message != '') {               
                     if ($success) { 
