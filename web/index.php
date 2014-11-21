15fbf9a44c62762e4584f3e56aa7c1a2e9bf4c1d
taban <cosmostaban@hotmail.com>
2014-11-20 21:28:33 -0800

    update

--- web/index.php
+++ web/index.php
@@ -10,18 +10,24 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
 ));
 
+// Register the Twig templating engine
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+  'twig.path' => __DIR__.'/../views',
+));
+
 // Our web handlers
 
-// $app->get('/', function() use($app) {
-//   $app['monolog']->addDebug('logging output.');
-//   return 'Hello';
-// });
-
-$app->get('/twig/{name}', function ($name) use ($app) {
-    return $app['twig']->render('index.twig', array(
-        'name' => $name,
-    ));
+$app->get('/', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return 'Hello';
 });
+
+$app->get('/twig/{name}', function($name) use($app) {
+  return $app['twig']->render('index.twig', array(
+    'name' => $name,
+  ));
+});
+
 
 $app->run();
 
