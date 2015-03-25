40ce1ae3991a69c56724fcfc32cfeb2c30e48358
taban@tabancosmos.com <taban@tabancosmos.com>
2015-03-25 13:34:03 -0700

    udpate

--- web/components/php/services.php
+++ web/components/php/services.php
@@ -2,27 +2,8 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <h3>Our Services</h3>
-            </div>
-            <div class="col-md-6 col-sm-6 wow bounceInLeft">
-                <p><span><i class="fa fa-desktop"></i></span></p>
-                <h4>Restaurants</h4>
-
-                <p>We provide restaurants customers' suggestions to help the restaurant take customers' suggestions
-                    seriously.
-                </p>
-            </div>
-            <div class="col-md-6 col-sm-6 wow bounceInUp">
-                <p><span><i class="fa fa-mobile"></i></span></p>
-                <h4>Application</h4>
-
-                <p>Our application informs customers of the menus, subsequently improving customer satisfaction and
-                    increse customer base for the restaurants.
-                </p
-            </div>
-            <!-- <div class="col-md-4 col-sm-4 wow bounceInRight">
-               <p><span><i class="fa fa-thumbs-up"></i></span></p>
-             </div>-->
+                <h3>Up coming Hackerthons</h3>
+                <p>Nothing yet, please come back again</p>
         </div>
     </div>
 </section>
