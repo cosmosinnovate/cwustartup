a223dfc64b1d5cf271e1cb0a32bc74a945f2973d
taban@tabancosmos.com <taban@tabancosmos.com>
2015-03-25 12:06:24 -0700

    update

--- web/components/php/contact.php
+++ web/components/php/contact.php
@@ -6,16 +6,17 @@
             </div>
         </div>
     </div>
-    <div class="fluid-wrapper">
-
-        <!-- IFRAME MAPS -->
-        <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
-                src="https://www.openstreetmap.org/#map=12/47.0000/-120.5422"
-                style="border: 1px solid black"></iframe>
-        <br/>
-        <small><a href="http://www.openstreetmap.org/?mlat=47.6242&amp;mlon=-122.3070#map=12/47.0000/-120.5423">View
-                Larger Map</a></small>
-    </div>
+    //Map goes here
+<!--    <div class="fluid-wrapper">-->
+<!---->
+<!--        <!-- IFRAME MAPS -->-->
+<!--        <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"-->
+<!--                src="http://www.openstreetmap.org/export/embed.html?bbox=-122.42305755615234%2C47.558920607496525%2C-122.19097137451172%2C47.68942806015855&amp;layer=mapnik&amp;marker=47.62421506209668%2C-122.30701446533202"-->
+<!--                style="border: 1px solid black"></iframe>-->
+<!--        <br/>-->
+<!--        <small><a href="https://thawing-bayou-8481.herokuapp.com"/>View-->
+<!--                Larger Map</a></small>-->
+<!--    </div>-->
     <div class="container">
         <div class="row">
             <div class="col-md-4 col-sm-4 wow bounceInLeft">
