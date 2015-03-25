f08d3b45cbff637edcc8f9a384f89819c56cf053
taban@tabancosmos.com <taban@tabancosmos.com>
2015-03-25 14:41:10 -0700

    updates

--- web/components/php/services.php
+++ web/components/php/services.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <h3>Up coming Hackerthons</h3>
+                <h3>Upcoming Hackerthon</h3>
                 <p>Nothing yet, please come back again</p>
         </div>
     </div>
