d091118ce294fc9323889a8a5d6dcc6e7d377847
taban@tabancosmos.com <taban@tabancosmos.com>
2015-03-25 13:22:35 -0700

    major changes

--- web/components/php/header.php
+++ web/components/php/header.php
@@ -54,7 +54,6 @@
                 <ul class="nav navbar-nav navbar-right">
                     <li class="active"><a href="#main">Home</a></li>
                     <li><a href="#about">About</a></li>
-<!--                    <li><a href="#portfolio">Mobile Apps</a></li>-->
                     <li><a href="#blogpost">Blogpost</a></li>
                     <li><a href="#team">Team</a></li>
                     <li><a href="#contact">Contact</a></li>
