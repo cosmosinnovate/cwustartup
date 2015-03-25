1cc47fb746be0e30b7b65f99a1771c05c7371de3
taban@tabancosmos.com <taban@tabancosmos.com>
2015-03-25 01:13:49 -0700

    update

--- web/components/php/team.php
+++ web/components/php/team.php
@@ -6,66 +6,22 @@
             </div>
             <div class="col-md-3 col-sm-3 wow bounceInLeft">
                 <div class="imgteam">
-                    <ul class="wow fadeInUp">
-                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                        <li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
-                        <li><a href="#"><i class="fa fa-twitter-square"></i></a></li>
-                    </ul>
+<!--                    <ul class="wow fadeInUp">-->
+<!--                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>-->
+<!--                        <li><a href="#"><i class="fa fa-facebook-square"></i></a></li>-->
+<!--                        <li><a href="#"><i class="fa fa-twitter-square"></i></a></li>-->
+<!--                    </ul>-->
                     <img src="images/p1.jpg" alt="p1">
                 </div>
-                <h4>Taban Cosmos</h4>
+                <h4 id="name">Taban Cosmos</h4>
 
                 <p></p>
             </div>
             <div class="col-md-3 col-sm-3 wow bounceInLeft">
                 <div class="imgteam">
-                    <ul class="wow fadeInUp">
-                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                        <li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
-                        <li><a href="#"><i class="fa fa-twitter-square"></i></a></li>
-                    </ul>
                     <img src="images/p2.jpg" alt="p2">
                 </div>
-                <h4>Osmond Gunarso</h4>
-
-                <p></p>
-            </div>
-            <div class="col-md-3 col-sm-3 wow bounceInRight">
-                <div class="imgteam">
-                    <ul class="wow fadeInUp">
-                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                        <li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
-                        <li><a href="#"><i class="fa fa-twitter-square"></i></a></li>
-                    </ul>
-                    <img src="images/p3.jpg" alt="p3">
-                </div>
-                <h4>Harsha Ravula</h4>
-
-                <p></p>
-            </div>
-            <div class="col-md-3 col-sm-3 wow bounceInRight">
-                <div class="imgteam">
-                    <ul class="wow fadeInUp">
-                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                        <li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
-                        <li><a href="#"><i class="fa fa-twitter-square"></i></a></li>
-                    </ul>
-                    <img src="images/p4.jpg" alt="p4">
-                </div>
-                <h4>John Quicksall</h4>
-
-                <p></p>
-            </div>
-            <div class="col-md-3 col-sm-3 wow bounceInRight">
-                <div class="imgteam">
-                    <ul class="wow fadeInUp">
-                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                        <li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
-                        <li><a href="#"><i class="fa fa-twitter-square"></i></a></li>
-                    </ul>
-                    <img src="images/p5.jpg" alt="p5">
-                </div>
-                <h4>Sahith Nareddy</h4>
+                <h4>Tommy Mathan</h4>
 
                 <p></p>
             </div>
