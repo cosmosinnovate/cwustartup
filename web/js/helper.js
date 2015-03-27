7a9bfed088c44303ad34822a2f10c0ffb1190e07
Cosmos-it <taban@tabancosmos.com>
2015-03-27 12:54:42 -0700

    update

--- web/js/helper.js
+++ web/js/helper.js
@@ -2,7 +2,8 @@
  * Created by Taban on 3/25/15.
  */
 
-angular.module("App", [])
-.constructor("Controller", ["$scope", function($scope){
-        $scope.name = items.about;
-    }]);
+var App = angular.module('App',[]);
+
+myApp.controller('Controller', ['$scope', function($scope) {
+    $scope.name = 'Hola!';
+}]);
