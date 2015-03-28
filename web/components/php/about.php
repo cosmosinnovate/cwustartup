01f833b08e9f33e3ac7b30b02e405bf4b857fda2
Cosmos-it <taban@tabancosmos.com>
2015-03-28 14:53:26 -0700

    Update

--- web/components/php/about.php
+++ web/components/php/about.php
@@ -4,7 +4,7 @@
             <div class="col-md-12">
                 <h3>About</h3>
             </div>
-            <div class="col-md-6 col-sm-6 wow bounceInLeft">
+            <div class="col-md-6 col-sm-6 wow bounceInLeft panel-body">
                 <p>{{about}}</p>
             </div>
             <div class="col-md-6 col-sm-6 wow bounceInRight">
