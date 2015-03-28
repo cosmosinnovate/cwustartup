a0e0a72a1a5ab57cc1d89e240b95bc6263a24e06
Cosmos-it <taban@tabancosmos.com>
2015-03-27 18:03:25 -0700

    Update

--- web/js/data.js
+++ web/js/data.js
@@ -5,21 +5,18 @@
 var data = {
     "about": "CWUSTARTUP dedicated to bring like minded students to come together and create \n" +
     "exciting technologies that solve problems around us.",
-    "members": [
-        {
+    "members": {
             "imageURL": "images/p1.jpg",
             "memberName": "Taban Cosmos",
             "memberDescription": "President of CWUSTARTUP \n" +
             "Computer Science Junior, I am passionate about what we do" +
-            "and I would like to make a difference here on campus "
-        },
-        {
+            "and I would like to make a difference here on campus ",
             "imageURL": "images/p2.jpg",
             "memberName": "Tommy Mathan",
             "memberDescription": "Vice President of CWUSTARTUP \n" +
             "Senior, I am passionate about what we do" +
             "and I would like to make a difference here on campus "
-        }],
+        },
 
     "projects": [],
     "events": []
