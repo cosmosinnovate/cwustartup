ad5dc42ffd9123f2876925b5372ab5b863a1a1d2
Cosmos-it <taban@tabancosmos.com>
2015-03-28 14:35:57 -0700

    Update

--- web/components/php/team.php
+++ web/components/php/team.php
@@ -5,7 +5,7 @@
                 <h3>CWUSTARTUP OFFICIALS</h3>
             </div>
             <div class="col-md-3 col-sm-3 wow bounceInLeft" ng-repeat="array in arrayData">
-                <div class="imgteam">
+                <div class="imgteam panel panel-default">
                     <img ng-src='{{array.image}}' alt="p1"/>
                     <h4 id="name">{{array.name}}</h4>
                     <p>{{array.description}}</p>
