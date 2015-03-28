8d052157ce95cf0ee92c731834db923684066376
Cosmos-it <taban@tabancosmos.com>
2015-03-27 20:06:46 -0700

    update

--- web/js/helper.js
+++ web/js/helper.js
@@ -2,24 +2,52 @@
  * Created by Taban on 3/25/15.
  */
 
-(function (angular) {
-    'use strict';
-    var myApp = angular.module('CWUSTARTUP', []);
+//(function (angular) {
+//    'use strict';
+//    var myApp = angular.module('CWUSTARTUP', []);
+//
+//    myApp.controller('About', ['$scope', function ($scope) {
+//        $scope.about = data.about;
+//    }]);
+//
+//    myApp.controller('Team', ['$scope', function ($scope) {
+//
+//        for (var i = 0; i <= 2; i++) {
+//            $scope.image = data.members.imageURL;
+//            $scope.name = data.members.memberName;
+//            $scope.description = data.members.memberDescription;
+//        }
+//
+//
+//    }]);
+//
+//
+//})(window.angular);
 
-    myApp.controller('About', ['$scope', function ($scope) {
-        $scope.about = data.about;
-    }]);
+(function(angular) {
+    var app = angular.module('CWUSTARTUP', []);
 
-    myApp.controller('Team', ['$scope', function ($scope) {
-
-        for (var i = 0; i <= 2; i++) {
-            $scope.image = data.members.imageURL;
-            $scope.name = data.members.memberName;
-            $scope.description = data.members.memberDescription;
+    //Create a service
+    app.service("cwustartupService", function ($http, $q)
+    {
+        var deferred = $q.defer();
+        $http.get('data.json').then(function (data)
+        {
+            deferred.resolve(data);
+        });
+        this.getTeamData = function ()
+        {
+            return deferred.promise;
         }
+    })
+        .controller("Team", function ($scope, cwustartupService)
+        {
+            var promise = cwustartupService.getTeamData();
+            promise.then(function (data)
+            {
 
-
-    }]);
-
-
+                $scope.members = data.data;
+                console.log($scope.members);
+            });
+        })
 })(window.angular);
