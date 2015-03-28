24182ed0f237a75759924097573f70cf50dd088c
Cosmos-it <taban@tabancosmos.com>
2015-03-28 13:48:09 -0700

    Update

--- web/js/cwuMember.js
+++ web/js/cwuMember.js
@@ -3,12 +3,12 @@ var data =
     "about": "CWUSTARTUP dedicated to bring like minded students to come together and create exciting technologies thatsolve problems around us.",
     "members": [
         {
-            "imageURL": "images/p1.jpg",
+            "image": "images/p1.jpg",
             "name": "Taban Cosmos",
             "description": "President of CWUSTARTUP Computer Science Junior, I am passionate about what we do and I would like to make a difference here on campus."
         },
         {
-            "imageURL": "images/p2.jpg",
+            "image": "images/p2.jpg",
             "name": "Tommy Mathan",
             "description": "Vice President of CWUSTARTUP Senior, I am passionate about what we do and I would like to make a difference here on campus "
         }
