50f8eb7d1c7cffc45d5bc5764b49024d08885ba4
Cosmos-it <taban@tabancosmos.com>
2015-03-29 03:03:42 -0700

    Update

--- web/components/php/team.php
+++ web/components/php/team.php
@@ -2,21 +2,26 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-12 text-center">
-                <h4>Our Amazing Team</h4>
-                <h5>The people who make cwu startup be cwu startup.</h5>
+                <h3>Our Amazing Team</h3>
+                <h4>The people who make cwu startup be cwu startup.</h4>
             </div>
         </div>
         <div class="row">
-                <div class="team-member col-md-3 col-sm-3 wow " ng-repeat="array in arrayData">
-                    <div class="imgteam">
-                        <img class="img-responsive img-circle" ng-src='{{array.image}}' alt="p1"/>
-                        <div class="team-member panel panel-default">
-                        <h4 id="name">{{array.name}}</h4>
-                        <h5>{{array.title}}</h5>
-                        <p>{{array.description}}</p>
+            <div class="team-member col-md-3 col-sm-3 wow " ng-repeat="array in arrayData">
+                <div class="imgteam">
+                    <img class="img-responsive img-circle" ng-src='{{array.image}}' alt="p1"/>
+
+                    <div class="team-member panel panel-default">
+                        <div class="panel-heading">
+                            <h4 id="name">{{array.name}}</h4>
+                            <h5>{{array.title}}</h5>
+                        </div>
+                        <div class="panel-body">
+                            <p>{{array.description}}</p>
                         </div>
                     </div>
                 </div>
+            </div>
         </div>
     </div>
     </div>
