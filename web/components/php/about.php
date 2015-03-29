e1dfbc04a11de9986ed39320ee40cd2d3178f737
Cosmos-it <taban@tabancosmos.com>
2015-03-29 02:33:10 -0700

    update

--- web/components/php/about.php
+++ web/components/php/about.php
@@ -8,5 +8,64 @@
                 <p class="panel-body">{{about}}</p>
             </div>
         </div>
+
+        <div class="row">
+            <div class="col-lg-12">
+                <ul class="timeline">
+                    <li>
+                        <div class="timeline-image">
+                            <img class="img-circle img-responsive" src="img/about/1.jpg" alt="">
+                        </div>
+                        <div class="timeline-panel">
+                            <div class="timeline-heading">
+                                <h4>January 2015</h4>
+                                <h4 class="subheading">Our Humble Beginnings</h4>
+                            </div>
+                            <div class="timeline-body">
+                                <p class="text-muted">Three students, Patrick Murphy, Taban Cosmos, and Thomas Mathan,
+                                    had the Idea, that central washington university needed a place where entrepeneurs
+                                    could meet.
+                                </p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="timeline-inverted">
+                        <div class="timeline-image">
+                            <img class="img-circle img-responsive" src="img/about/2.jpg" alt="">
+                        </div>
+                        <div class="timeline-panel">
+                            <div class="timeline-heading">
+                                <h4>Feburary 2015</h4>
+                                <h4 class="subheading">The club is born</h4>
+                            </div>
+                            <div class="timeline-body">
+                                <p class="text-muted">With this gumption and passion, CWU Startup was born</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="timeline-inverted">
+                        <div class="timeline-image">
+                            <img class="img-circle img-responsive" src="img/about/4.jpg" alt="">
+                        </div>
+                        <div class="timeline-panel">
+                            <div class="timeline-heading">
+                                <h4>2015 and beyond</h4>
+                                <h4 class="subheading">Phase Two Expansion</h4>
+                            </div>
+                            <div class="timeline-body">
+                                <p class="text-muted">From students to moguls</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="timeline-inverted">
+                        <div class="timeline-image">
+                            <h4>Be Part
+                                <br>Of Our
+                                <br>Story!</h4>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
 </section>
