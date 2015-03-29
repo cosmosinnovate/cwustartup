b55c36893bc40c9d63e03deb5a622a2a6fe36efc
Cosmos-it <taban@tabancosmos.com>
2015-03-28 21:44:39 -0700

    Update

--- web/components/php/about.php
+++ web/components/php/about.php
@@ -7,18 +7,8 @@
             <div class="section-subheading text-muted">
                 <p class="panel-body">{{about}}</p>
             </div>
-            <div class="col-md-6 col-sm-6 wow bounceInRight">
-                <p></p>
-                <p class="icon-about">
-                    <a href=""><i
-                            class="fa fa-facebook-square"></i></a>
-                    <a href="#"><i class="fa fa-twitter-square"></i></a>
-                    <a href="#"><i class="fa fa-google-plus-square"></i></a>
-                    <a href="#"><i class="fa fa-pinterest-square"></i></a>
-                </p>
-
-            </div>
         </div>
+    </div>
     </div>
     </div>
 </section>
