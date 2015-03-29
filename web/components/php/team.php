cfc7ea9687cfc863bcdf3084ca5d4e34812220c9
Cosmos-it <taban@tabancosmos.com>
2015-03-29 02:05:58 -0700

    Update

--- web/components/php/team.php
+++ web/components/php/team.php
@@ -3,13 +3,12 @@
         <div class="row">
             <div class="col-lg-12 text-center">
                 <h3 class="section-heading">Our Amazing Team</h3>
-
                 <h2 class="section-subheading text-muted">The people who make cwu startup be cwu startup.</h2>
             </div>
         </div>
         <div class="row">
             <div class="col-sm-4">
-                <div class="team-member " ng-repeat="array in arrayData">
+                <div class="team-member col-md-3 col-sm-3 wow " ng-repeat="array in arrayData">
                     <div class="imgteam">
                         <img class="img-responsive img-circle" ng-src='{{array.image}}' alt="p1"/>
                         <h4 id="name">{{array.name}}</h4>
