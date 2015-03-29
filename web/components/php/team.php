1415d98f01fdbadf7337994deddd75dda8ef247b
Cosmos-it <taban@tabancosmos.com>
2015-03-29 02:19:14 -0700

    Update

--- web/components/php/team.php
+++ web/components/php/team.php
@@ -8,11 +8,13 @@
         </div>
         <div class="row">
                 <div class="team-member col-md-3 col-sm-3 wow " ng-repeat="array in arrayData">
-                    <div class="imgteam panel panel-default">
+                    <div class="imgteam">
                         <img class="img-responsive img-circle" ng-src='{{array.image}}' alt="p1"/>
+                        <div class="panel panel-default"
                         <h4 id="name">{{array.name}}</h4>
                         <h5>{{array.title}}</h5>
                         <p>{{array.description}}</p>
+                        </div>
                     </div>
                 </div>
         </div>
