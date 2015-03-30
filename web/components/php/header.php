a1a32a112028b2d464372f57310d7a9b7a29876b
Cosmos-it <taban@tabancosmos.com>
2015-03-29 20:26:36 -0700

    Update

--- web/components/php/header.php
+++ web/components/php/header.php
@@ -46,29 +46,67 @@
 
 <body>
 <div id="page">
-    <div class="site-navbar navbar-fixed-top" role="navigation">
+<!--    <div class="site-navbar navbar-fixed-top" role="navigation">-->
+<!--        <div class="container">-->
+<!--            <div class="navbar-header page scroll">-->
+<!--                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">-->
+<!--                    <span class="sr-only">Toggle navigation</span>-->
+<!--                    <span class="icon-bar"></span>-->
+<!--                    <span class="icon-bar"></span>-->
+<!--                    <span class="icon-bar"></span>-->
+<!--                </button>-->
+<!--                <!-- Logo  -->-->
+<!--                <a class="navbar-brand page-scrol" href="index.html"><h3><strong>CWU Startup</strong></h3></a>-->
+<!--            </div>-->
+<!--            <!-- Navigation -->-->
+<!--            <div class="navbar-collapse collapse">-->
+<!--                <ul class="nav navbar-nav navbar-right">-->
+<!--                    <li class="active"><a href="#main">Home</a></li>-->
+<!--                    <li><a href="#about">About</a></li>-->
+<!--                    <li><a href="#services">Events</a></li>-->
+<!--                    <li><a href="#team">Team</a></li>-->
+<!--                    <li><a href="#contact">Contact</a></li>-->
+<!--                </ul>-->
+<!--            </div>-->
+<!--            <!-- .nav-collapse -->-->
+<!--        </div>-->
+<!--    </div>-->
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-fixed-top">
         <div class="container">
-            <div class="navbar-header page scroll">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <!-- Logo  -->
-                <a class="navbar-brand page-scrol" href="index.html"><h3><strong>CWU Startup</strong></h3></a>
+                <a class="navbar-brand page-scroll" href="#page-top">Central Washington Startup Club</a>
             </div>
-            <!-- Navigation -->
-            <div class="navbar-collapse collapse">
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                    <li class="active"><a href="#main">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#services">Events</a></li>
-                    <li><a href="#team">Team</a></li>
-                    <li><a href="#contact">Contact</a></li>
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#about">About</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#FutureEvents">Future Events</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#team">Team</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">Contact</a>
+                    </li>
                 </ul>
             </div>
-            <!-- .nav-collapse -->
+            <!-- /.navbar-collapse -->
         </div>
-    </div>
+        <!-- /.container-fluid -->
+    </nav>
 
