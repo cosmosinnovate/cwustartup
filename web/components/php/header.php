9cb3bbf66ddf6b6490a6eca4c3102598ba58910f
Cosmos-it <taban@tabancosmos.com>
2015-03-31 13:40:03 -0700

    Update

--- web/components/php/header.php
+++ web/components/php/header.php
@@ -27,29 +27,32 @@
 </head>
 
 <body>
-<div id="page">
-    <div class="site-navbar navbar navbar-fixed-top" role="navigation">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <!-- Logo  -->
-                <a class="navbar-brand" href="index.html"><h3><strong>CWU Startup</strong></h3></a>
+<div class="navbar-wrapper">
+    <div class="container">
+        <nav class="navbar navbar-inverse navbar-static-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <!-- Logo  -->
+                    <a class="navbar-brand" href="index.html"><h3><strong>CWU Startup</strong></h3></a>
+                </div>
+                <!-- Navigation -->
+                <div class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="active"><a href="#main">Home</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#services">Events</a></li>
+                        <li><a href="#team">Team</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                </div>
+                <!-- .nav-collapse -->
             </div>
-            <!-- Navigation -->
-            <div class="navbar-collapse collapse">
-                <ul class="nav navbar-nav navbar-right">
-                    <li class="active"><a href="#main">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#services">Events</a></li>
-                    <li><a href="#team">Team</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                </ul>
-            </div>
-            <!-- .nav-collapse -->
-        </div>
+        </nav>
     </div>
+</div>
