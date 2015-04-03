d4e5a23356dfb40ec5ac34226d2e7990cf8394e0
Patrick Murphy <patrick@patrickmurphyphoto.com>
2015-04-02 18:45:18 -0700

    Index.php > Condense to one set of PHP tags for performance / readability

--- web/index.php
+++ web/index.php
@@ -1,7 +1,9 @@
-<?php include_once 'components/php/header.php'; ?>
-<?php include_once 'components/php/carousel.php'; ?>
-<?php include_once 'components/php/about.php'; ?>
-<?php include_once 'components/php/FutureEvents.php'; ?>
-<?php include_once 'components/php/team.php'; ?>
-<?php include_once 'components/php/contact.php'; ?>
-<?php require_once 'components/php/footer.php'; ?>
+<?php 
+
+include_once 'components/php/header.php';
+include_once 'components/php/carousel.php';
+include_once 'components/php/about.php';
+include_once 'components/php/FutureEvents.php';
+include_once 'components/php/team.php';
+include_once 'components/php/contact.php';
+require_once 'components/php/footer.php';
