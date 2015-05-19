089f97676fea4b93f108224999dc52ac75600246
Whatsonplatestartup <cosmostaban@gmail.com>
2015-05-18 18:19:35 -0700

    Updated Services button

--- web/components/php/services.php
+++ web/components/php/services.php
@@ -4,7 +4,7 @@
         <div class="row">
             <div class="col-lg-12 text-center">
                 <h2 class="section-heading">REGISTRATION FOR HACKATHON</h2>
-                <div class="btn-info"
+                <div class="btn-info">
                     <a href="http://patrickmurphywebdesign.com/Projects/cwustartup/">REGISTER HERE!</a>
                 </div>
             </div>
