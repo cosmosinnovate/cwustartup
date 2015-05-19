75cfd6952b8fbf55b40efa3c6ceacfd50de8df98
Whatsonplatestartup <cosmostaban@gmail.com>
2015-05-18 18:12:42 -0700

    udpates

--- web/components/php/services.php
+++ web/components/php/services.php
@@ -4,7 +4,9 @@
         <div class="row">
             <div class="col-lg-12 text-center">
                 <h2 class="section-heading">REGISTRATION FOR HACKATHON</h2>
-                    <a class="btn-info" href="http://patrickmurphywebdesign.com/Projects/cwustartup/">REGISTER HERE!</a>
+                <div class="btn-info"
+                    <a href="http://patrickmurphywebdesign.com/Projects/cwustartup/">REGISTER HERE!</a>
+                </div>
             </div>
         </div>
     </div>
