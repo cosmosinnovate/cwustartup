e1605c9b429994bbd5d7592750309769d8218ba5
Whatsonplatestartup <cosmostaban@gmail.com>
2015-05-18 17:53:32 -0700

    Updates

--- web/components/php/services.php
+++ web/components/php/services.php
@@ -4,7 +4,9 @@
         <div class="row">
             <div class="col-lg-12 text-center">
                 <h2 class="section-heading">Future Events</h2>
-                <div class="btn"><a href="http://patrickmurphywebdesign.com/Projects/cwustartup/">REGISTER HERE!</a>  </div>
+                <div class="btn-default">
+                    <a href="http://patrickmurphywebdesign.com/Projects/cwustartup/">REGISTER HERE!</a>
+                </div>
             </div>
         </div>
     </div>
